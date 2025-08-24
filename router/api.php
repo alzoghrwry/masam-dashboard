00c82d464a5fe928b37e1e4a5f1a75dec8683176
ahmed <ahmedalzoghrory@gmail.com>
2025-08-24 16:29:48 +0300

    chore(api): update API router

--- router/api.php
+++ router/api.php
@@ -11,7 +11,7 @@ $router->get('/', [AuthController::class, 'index']);
 $router->post('/api/login', [AuthController::class,'login']);
 $router->post('/api/logout', [AuthController::class,'logout']);
 
-// Users
+
 $router->get('/api/users', [UserController::class,'index']);
 $router->post('/api/users', [UserController::class,'store']);
 $router->put('/api/users/{id}', [UserController::class,'update']);
